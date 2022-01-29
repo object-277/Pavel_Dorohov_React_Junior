6337ccac66cd5252b2fba07f73a4badd50bb918c
Pavel <pavelsdorohovs@gmail.com>
2022-01-29 14:43:00 +0000

    init app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-  render(<p>Hello</p>);
+  render() {
+    return (<p>Hello</p>);
+  }
+    
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
